77a68d8bbe29f5e27be4dbff8ab28b46e0c72dbc
Dainius Strazdas <dainius19961996@gmail.com>
2023-05-16 15:03:59 +0300

    Finished work section

--- portfolio/src/components/Job.js
+++ portfolio/src/components/Job.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "../styles/jobs.css";
 
-const Job = ({ id, title, desc }) => {
+const Job = ({ id, company, role, when, desc }) => {
   return (
     <div className="job-panels">
       <div className="job-panel">
-        <h3>{title}</h3>
+        <h3>
+          {role}
+          <span> @ {company}</span>
+        </h3>
+        <p className="when">{when}</p>
         <ul>
-          <li>{desc}</li>
+          {desc.map((description, index) => (
+            <li key={index}>{description}</li>
+          ))}
         </ul>
       </div>
     </div>
--- portfolio/src/components/Jobs.js
+++ portfolio/src/components/Jobs.js
@@ -10,16 +10,53 @@ const Jobs = () => {
   };
 
   const jobs = [
-    { id: 1, title: "First", desc: "First work" },
-    { id: 2, title: "Second", desc: "Work" },
-    { id: 3, title: "Third", desc: "Work" },
-    { id: 4, title: "Fourth", desc: "Work" },
-    { id: 5, title: "Fifth", desc: "Work" },
+    {
+      id: 1,
+      company: "Fitsout",
+      role: "Team Leader",
+      when: "April - May 2023",
+      desc: [
+        "Checking product quality, ensuring that quality standards were met",
+        "Make sure all people have a job and materials for product assembly",
+      ],
+    },
+    {
+      id: 2,
+      company: "Narbutas",
+      role: "Team Leader",
+      when: "January 2021 - October 2022",
+      desc: [
+        "Was responsible for overseeing and coordinating the day-to-day operations of the production line",
+        "Managed a team of 24 employees",
+        "Ensuring that quality standards were met and that the production line was running efciently",
+      ],
+    },
+    {
+      id: 3,
+      company: "Narbutas",
+      role: "Work organizer",
+      when: "September 2017 - January 2021",
+      desc: [
+        "Had to solve problems that have arisen",
+        "Communicate with other people, leading that the products the production line ran smoothly, and that deadlines were met",
+      ],
+    },
+    {
+      id: 4,
+      company: "Narbutas",
+      role: "Cabinet assembler-packer",
+      when: "August 2015 - September 2017",
+      desc: [
+        "Had to assemble the cabinets, when needed go to pack them",
+        "Memorize the color codes of production",
+        "Speed and critical thinking was most important thing",
+      ],
+    },
   ];
 
   return (
     <section className="experience">
-      <h2 className="numbered-heading">Experience I've Got</h2>
+      <h2 className="numbered-heading">Where I've Worked</h2>
       <div className="innerr">
         <div className="job-list">
           {jobs.map((job) => (
@@ -30,7 +67,7 @@ const Jobs = () => {
               }`}
               onClick={() => handleJobButtonClick(job.id)}
             >
-              <span>{job.title}</span>
+              <span>{job.company}</span>
             </button>
           ))}
         </div>
@@ -40,7 +77,13 @@ const Jobs = () => {
               (job) =>
                 job.id === selectedJobId && (
                   <span key={job.id} className="show">
-                    <Job id={job.id} title={job.title} desc={job.desc} />
+                    <Job
+                      id={job.id}
+                      company={job.company}
+                      role={job.role}
+                      when={job.when}
+                      desc={job.desc}
+                    />
                   </span>
                 )
             )}
