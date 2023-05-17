034eeaeff7f1dcd9538dada2730e5a0a90613907
Dainius Strazdas <dainius19961996@gmail.com>
2023-05-17 20:16:30 +0300

    Added socials and email sections, fixed where sections didn't displayed proper section number

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,24 +1,50 @@
 import React from "react";
 import "../styles/hero.css";
 
+import { FiGithub } from "react-icons/fi";
+import { FiLinkedin } from "react-icons/fi";
+
 const Hero = () => {
   return (
-    <main>
-      <section>
-        <h1>Hi, my name is</h1>
-        <h2 className="big-heading">Dainius Strazdas.</h2>
-        <h3 className="big-heading">I build things for the web.</h3>
-        <p>
-          I'm a front end developer with a passion for creating beautiful,
-          intuitive interfaces that make users lives easier. Whether you're
-          looking to build a new website or improve an existing one, I have the
-          skills and expertise to help bring your vision to life.
-        </p>
-        <a className="big_button" href="">
-          Check out my projects!
-        </a>
-      </section>
-    </main>
+    <>
+      <div className="side-left">
+        <ul className="side-socials">
+          <li>
+            <a href="" target="_blank" rel="noreferrer">
+              <FiGithub style={{ width: "20px", height: "20px" }} />
+            </a>
+          </li>
+          <li>
+            <a href="" target="_blank" rel="noreferrer">
+              <FiLinkedin style={{ width: "20px", height: "20px" }} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="side-right">
+        <div className="side-email">
+          <a href="" target="_blank" rel="noreferrer">
+            contact@dainiusstrazdas.tech
+          </a>
+        </div>
+      </div>
+      <main>
+        <section>
+          <h1>Hi, my name is</h1>
+          <h2 className="big-heading">Dainius Strazdas.</h2>
+          <h3 className="big-heading">I build things for the web.</h3>
+          <p>
+            I'm a front end developer with a passion for creating beautiful,
+            intuitive interfaces that make users lives easier. Whether you're
+            looking to build a new website or improve an existing one, I have
+            the skills and expertise to help bring your vision to life.
+          </p>
+          <a className="big_button" href="">
+            Check out my projects!
+          </a>
+        </section>
+      </main>
+    </>
   );
 };
 
