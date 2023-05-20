8f58bd5922842adb7319c1567cc7bf09d42e14c5
Dainius Strazdas <dainius19961996@gmail.com>
2023-05-20 17:31:23 +0300

    Added hamburger menu, added media queries for navigation

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -9,6 +9,7 @@ import resume from "../images/resume.pdf";
 const Nav = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
   const [visible, setVisible] = useState(true);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -60,6 +61,27 @@ const Nav = () => {
     }
   );
 
+  const [resumemobileRef, resumemobileSprings] = useInView(
+    () => ({
+      from: {
+        opacity: 0,
+        transform: "translateY(-100%)",
+      },
+      to: {
+        opacity: 1,
+        transform: "translateY(0%)",
+      },
+    }),
+    {
+      rootMargin: "0% 0%",
+      once: true,
+    }
+  );
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <nav className={navClass}>
       <a
@@ -81,6 +103,33 @@ const Nav = () => {
           </li>
         ))}
         <animated.div ref={resumeRef} style={resumeSprings}>
+          <a
+            className="resume"
+            href={resume}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Resume
+          </a>
+        </animated.div>
+      </ol>
+
+      <div className="ham-box" onClick={handleOpen}>
+        <div className={open ? "ham-open" : "ham-inner"}></div>
+      </div>
+      <ol className={open ? "mobile" : "none"}>
+        {navItems.map((item, index) => (
+          <li
+            key={index}
+            style={{
+              transitionDelay: `${item.delay}ms`,
+              transform: mounted ? "translateY(0%)" : "translateY(-200%)",
+            }}
+          >
+            <a href={`#${item.text.toLowerCase()}`}>{item.text}</a>
+          </li>
+        ))}
+        <animated.div ref={resumemobileRef} style={resumemobileSprings}>
           <a
             className="resume"
             href={resume}
